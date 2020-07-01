ea241da522f0a9a236f349ff756bb6070aeb808f
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-07-01 13:45:50 +0200

    Create ShinyModule.R
    first RShiny-code to plot positions on a simple coastline plot. can add variable empty edges to the plot, because for small range coastlines might only help if zooming out

---
+++ ShinyModule.R
@@ -0,0 +1,41 @@
+library(move)
+library(shiny)
+library(maps)
+
+shinyModuleUserInterface <- function(id, label) {
+  ns <- NS(id)
+  
+  tagList(
+    titlePanel("Simple map of the positions of your animals"),
+    sliderInput(inputId = ns("num"), 
+                label = "Choose a margin size", 
+                value = 2, min = 1, max = 30),
+    plotOutput(ns("map"))
+  )
+}
+
+shinyModuleConfiguration <- function(id, input) {
+  ns <- NS(id)
+  configuration <- list()
+
+  configuration
+}
+
+shinyModule <- function(input, output, session, data) {
+  dataObj <- reactive({ data })
+  current <- reactiveVal(data)
+  
+  lonObj <- reactive({
+    coordinates(dataObj())[,1]
+  })
+  latObj <- reactive({
+    coordinates(dataObj())[,2]
+  })
+  
+  output$map <- renderPlot({
+    map("world",xlim=c(min(lonObj())-input$num,max(lonObj())+input$num),ylim=c(min(latObj())-input$num,max(latObj())+input$num))
+    points(lonObj(),latObj(),col=factor(trackId(current())),pch=20)  
+  })
+  
+  return(reactive({ current() }))
+}
